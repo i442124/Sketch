3f46d19ac513875f61aded82b60aad8638f183be
Rick <442124@student.fontys.nl>
2020-12-03 17:54:00 +0100

    Update Color components

--- src/Sketch.Shared/Data/Color.cs
+++ src/Sketch.Shared/Data/Color.cs
@@ -2,5 +2,36 @@
 {
     public struct Color
     {
+        public byte R { get; }
+
+        public byte G { get; }
+
+        public byte B { get; }
+
+        public Color(byte r, byte g, byte b)
+        {
+            this = FromRGB(r, g, b);
+        }
+
+        public Color(double hue, double saturation, double luminosity)
+        {
+            this = FromHSL(hue, saturation, luminosity);
+        }
+
+        public double Hue => ColorUtilities.GetHue(this);
+
+        public double Saturation => ColorUtilities.GetSaturation(this);
+
+        public double Luminosity => ColorUtilities.GetLuminosity(this);
+
+        public static Color FromRGB(byte r, byte g, byte b)
+        {
+            return ColorUtilities.FromRGB(r, g, b);
+        }
+
+        public static Color FromHSL(double hue, double saturation, double luminosity)
+        {
+            return ColorUtilities.FromHSL(hue, saturation, luminosity);
+        }
     }
 }
