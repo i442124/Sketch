c261d01f0b6c6c4becf8178e218b5e996110d028
Rick <442124@student.fontys.nl>
2020-12-03 17:58:51 +0100

    Implement tool settings

---
+++ src/Sketch.Shared/Models/ToolSettings/ColorSettings.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+
+using Sketch.Shared.Data;
+
+namespace Sketch.Shared.Models
+{
+    public class ColorSettings : IColorSettings
+    {
+        public Color Color { get; private set; }
+
+        public void SetHue(float hue)
+        {
+            Color = Color.FromHSL(hue, Color.Saturation, Color.Luminosity);
+        }
+
+        public Task SetHueAsync(float hue)
+        {
+            return Task.Run(() => SetHue(hue));
+        }
+
+        public void SetSaturation(float saturation)
+        {
+            Color = Color.FromHSL(Color.Hue, saturation, Color.Luminosity);
+        }
+
+        public Task SetSaturationAsync(float saturation)
+        {
+            return Task.Run(() => SetSaturation(saturation));
+        }
+
+        public void SetLuminosity(float luminosity)
+        {
+            Color = Color.FromHSL(Color.Hue, Color.Saturation, luminosity);
+        }
+
+        public Task SetLuminosityAsync(float luminosity)
+        {
+            return Task.Run(() => SetLuminosity(luminosity));
+        }
+    }
+}
