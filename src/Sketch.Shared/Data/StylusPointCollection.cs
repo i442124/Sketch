0508000a9a05aa61119a0f285dcd7145af9bb92c
Rick <442124@student.fontys.nl>
2020-12-03 23:12:21 +0100

    Update StylusPointCollection

--- src/Sketch.Shared/Data/StylusPointCollection.cs
+++ src/Sketch.Shared/Data/StylusPointCollection.cs
@@ -6,5 +6,13 @@ namespace Sketch.Shared.Data
 {
     public class StylusPointCollection : Collection<StylusPoint>
     {
+        public StylusPointCollection()
+        {
+        }
+
+        public StylusPointCollection(IList<StylusPoint> list)
+            : base(list)
+        {
+        }
     }
 }
