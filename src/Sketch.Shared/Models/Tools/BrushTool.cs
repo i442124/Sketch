239400e9061e387c03c68335ae2bd8fc5d4a1dd5
Rick <442124@student.fontys.nl>
2020-12-03 18:13:29 +0100

    Implement sketch tools

---
+++ src/Sketch.Shared/Models/Tools/BrushTool.cs
@@ -0,0 +1,25 @@
+﻿using Sketch.Shared;
+using Sketch.Shared.Data;
+
+namespace Sketch.Shared.Models
+{
+    public class BrushTool : IBrushTool
+    {
+        private readonly IBrushSettings _brushSettings;
+        private readonly IColorSettings _colorSettings;
+
+        public BrushTool(
+            IBrushSettings brushSettings,
+            IColorSettings colorSettings)
+        {
+            _brushSettings = brushSettings;
+            _colorSettings = colorSettings;
+        }
+
+        public float Size => _brushSettings.Size;
+
+        public Color Color => _colorSettings.Color;
+
+        public float Opacity => _brushSettings.Opacity;
+    }
+}
---
+++ src/Sketch.Shared/Models/Tools/PaintBucketTool.cs
@@ -0,0 +1,23 @@
+﻿using Sketch.Shared;
+using Sketch.Shared.Data;
+
+namespace Sketch.Shared.Models
+{
+    public class PaintBucketTool : IPaintBucketTool
+    {
+        private readonly IBrushSettings _brushSettings;
+        private readonly IColorSettings _colorSettings;
+
+        public PaintBucketTool(
+            IBrushSettings brushSettings,
+            IColorSettings colorSettings)
+        {
+            _brushSettings = brushSettings;
+            _colorSettings = colorSettings;
+        }
+
+        public Color Color => _colorSettings.Color;
+
+        public float Opacity => _brushSettings.Opacity;
+    }
+}
