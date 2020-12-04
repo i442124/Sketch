22d78620bc030af25a66483340c85e6a8b7c3e9e
Rick <442124@student.fontys.nl>
2020-12-04 01:08:18 +0100

    Add default colors, color palettes

---
+++ src/Sketch.Shared/Data/ColorPalette.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Sketch.Shared.Data
+{
+    public class ColorPalette
+    {
+        public IEnumerable<Color> GetMixtureOfTints(Color color, int count)
+        {
+            var hue = color.Hue;
+            var sat = color.Saturation;
+            var lum = color.Luminosity;
+
+            var steps = count + 1;
+            var stepSize = (1 - color.Luminosity) / steps;
+
+            for (int i = 1; i < steps; i++)
+            {
+                lum += stepSize;
+                yield return Color.FromHSL(hue, sat, lum);
+            }
+        }
+
+        public IEnumerable<Color> GetMixturesOfShades(Color color, int count)
+        {
+            var hue = color.Hue;
+            var sat = color.Saturation;
+            var lum = color.Luminosity;
+
+            var steps = count + 1;
+            var stepSize = color.Luminosity / steps;
+
+            for (int i = 1; i < steps; i++)
+            {
+                lum -= stepSize;
+                yield return Color.FromHSL(hue, sat, lum);
+            }
+        }
+    }
+}
