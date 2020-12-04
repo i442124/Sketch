8d979528a7f68fc12343cbec5e3ab0ef8229c9e2
Rick <442124@student.fontys.nl>
2020-12-04 03:45:00 +0100

    Add Subscription endpoints

---
+++ src/Sketch.WebServer/Controllers/Subscriptions/SubscribeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Sketch.WebServer;
+using Sketch.WebServer.Services;
+
+namespace Sketch.WebServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SubscribeController : Controller
+    {
+        private readonly IBroadcastService _broadcastService;
+
+        public SubscribeController(IBroadcastService broadcastService)
+        {
+            _broadcastService = broadcastService;
+        }
+
+        [HttpGet("{subscriberId}/{channel}")]
+        public async Task<IActionResult> Get(string subscriberId, string channel)
+        {
+            await _broadcastService.SubscribeAsync(subscriberId, channel);
+            return Ok();
+        }
+    }
+}
