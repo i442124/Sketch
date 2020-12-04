636d9057cea98f92478bad5db5d8bafaec4b0fde
Rick <442124@student.fontys.nl>
2020-12-04 03:46:09 +0100

    Add SKChannelComponent, SKChannelSelection

---
+++ src/Sketch.WebApp/Views/Subscriptions/SKChannelSelection.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Components;
+
+using Sketch.Shared.Data;
+using Sketch.WebApp.Components;
+
+namespace Sketch.WebApp.Views
+{
+    public partial class SKChannelSelection : SKChannelComponent
+    {
+        public string Channel { get; private set; }
+
+        private SemaphoreSlim Semaphore { get; set; }
+        = new SemaphoreSlim(initialCount: 1, maxCount: 1);
+
+        private async Task OnSelectionChangedAsync(ChangeEventArgs e)
+        {
+            await Semaphore.WaitAsync();
+
+            try
+            {
+                if (!string.IsNullOrEmpty(Channel))
+                {
+                    await UnsubscribeAsync(Channel);
+                }
+
+                await SubscribeAsync(Channel = (string)e.Value);
+            }
+            finally
+            {
+                Semaphore.Release();
+            }
+        }
+    }
+}
