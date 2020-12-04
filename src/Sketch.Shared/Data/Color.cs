5a88c95674d596246e9889be11650c523da3532f
Rick <442124@student.fontys.nl>
2020-12-04 03:43:56 +0100

    Add setters to Color

--- src/Sketch.Shared/Data/Color.cs
+++ src/Sketch.Shared/Data/Color.cs
@@ -2,17 +2,11 @@
 {
     public struct Color
     {
-        public byte R { get; private set; }
+        public byte R { get; set; }
 
-        public byte G { get; private set; }
+        public byte G { get; set; }
 
-        public byte B { get; private set; }
-
-        public double Hue => ColorUtilities.GetHue(this);
-
-        public double Saturation => ColorUtilities.GetSaturation(this);
-
-        public double Luminosity => ColorUtilities.GetLuminosity(this);
+        public byte B { get; set; }
 
         public Color(byte r, byte g, byte b)
         {
@@ -22,6 +16,24 @@
         public Color(double hue, double saturation, double luminosity)
         {
             this = FromHSL(hue, saturation, luminosity);
+        }
+
+        public double Hue
+        {
+            get { return ColorUtilities.GetHue(this); }
+            set { this = FromHSL(value, Saturation, Luminosity); }
+        }
+
+        public double Saturation
+        {
+            get { return ColorUtilities.GetSaturation(this); }
+            set { this = FromHSL(Hue, value, Luminosity); }
+        }
+
+        public double Luminosity
+        {
+            get { return ColorUtilities.GetLuminosity(this); }
+            set { this = FromHSL(Hue, Saturation, value); }
         }
 
         public string ToRGBString()
