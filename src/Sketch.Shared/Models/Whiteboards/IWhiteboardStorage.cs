cace19f35629a62f7d93ee1fda383945b1f74de5
Rick <442124@student.fontys.nl>
2020-12-06 23:22:50 +0100

    Create IWhiteboardStorage

---
+++ src/Sketch.Shared/Models/Whiteboards/IWhiteboardStorage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Sketch.Shared.Data;
+using Sketch.Shared.Data.Ink;
+
+namespace Sketch.Shared.Models
+{
+    public interface IWhiteboardStorage
+    {
+        void Pop();
+
+        Task PopAsync();
+
+        void Push(Stroke stroke);
+
+        Task PushAsync(Stroke stroke);
+
+        void Push(Wipe wipe);
+
+        Task PushAsync(Wipe wipe);
+
+        void Push(Fill fill);
+
+        Task PushAsync(Fill fill);
+    }
+}
---
+++ src/Sketch.Shared/Models/Whiteboards/WhiteboardStorage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Sketch.Shared.Data.Ink;
+using Sketch.Shared.Services;
+
+namespace Sketch.Shared.Models
+{
+    public class WhiteboardStorage : IWhiteboardStorage
+    {
+        public void Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PopAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(Stroke stroke)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PushAsync(Stroke stroke)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(Wipe wipe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PushAsync(Wipe wipe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(Fill fill)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PushAsync(Fill fill)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
