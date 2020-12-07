025f9d22ca35ac9c81f7f40d7cc277356822081d
Rick <442124@student.fontys.nl>
2020-12-07 18:17:03 +0100

    Add SKClearButton, SKUndoButton

---
+++ src/Sketch.WebApp/Views/Actions/SKClearButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+using Sketch.WebApp;
+using Sketch.WebApp.Components;
+
+namespace Sketch.WebApp.Views
+{
+    public partial class SKClearButton : SKClearComponent
+    {
+        private Task OnClickAsync(MouseEventArgs e)
+        {
+            return ClearAsync();
+        }
+    }
+}
---
+++ src/Sketch.WebApp/Views/Actions/SKUndoButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+using Sketch.WebApp;
+using Sketch.WebApp.Components;
+
+namespace Sketch.WebApp.Views
+{
+    public partial class SKUndoButton : SKUndoComponent
+    {
+        private Task OnClickAsync(MouseEventArgs e)
+        {
+            return UndoAsync();
+        }
+    }
+}
